ac6336d1aa35e0175a841d64c7c3eca192717d55
Werner Sosa <2024161143@ub.edu.bz>
2026-01-31 15:28:22 -0600

    Complete Part 1

---
+++ exercise1.js
@@ -0,0 +1,8 @@
+function calculateArea(length, width) {
+    return length * width;
+}
+
+console.log(calculateArea(5, 10));
+console.log(calculateArea(7, 3));
+console.log(calculateArea(4, 4));
+
---
+++ exercise2.js
@@ -0,0 +1,8 @@
+const calculatePerimiter = function(length, width) {
+    return 2 * (length + width);
+}
+
+console.log(calculatePerimiter(5, 10));
+console.log(calculatePerimiter(7, 3));
+console.log(calculatePerimiter(4, 4));
+
